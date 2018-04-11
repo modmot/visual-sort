343b4f34150539bbe6b590d0e43b6b73e5fda05c
modmot <matthias.m.oberholzer@gmail.com>
2018-04-11 19:52:35 +0200

    refactored script.js

--- fancy/assets/js/script.js
+++ fancy/assets/js/script.js
@@ -1,17 +1,13 @@
-"use strict";
-let gc = document.getElementById("canvas").getContext("2d");
-console.log(document.getElementById("canvas").getContext("2d"));
-
-let width = document.getElementById("canvas").getAttribute("width");
-let height = document.getElementById("canvas").getAttribute("height");
+const gc = document.getElementById("canvas").getContext("2d");
+const width = document.getElementById("canvas").getAttribute("width");
+const height = document.getElementById("canvas").getAttribute("height");
 
 // time measure (duration)
 let start;
 let elapsed;
 
 // the Array to sort
-let points = new Array(600);
-
+const points = new Array(600);
 
 function paint() {
     gc.fillStyle = "black";
@@ -31,9 +27,6 @@ function shuffle() {
     }
 }
 
-
-//
-// bubblesort
 // --------------------------------------------------------------
 function start_bubblesort() {
     start = new Date().getTime();
@@ -42,7 +35,7 @@ function start_bubblesort() {
 
 function bubblesort() {
     let fertig = true;
-    let len = points.length;
+    const len = points.length;
     for (let i = 0; i < len - 1; i++) {
         if (points[i] > points[i + 1]) {
             let help;
@@ -74,12 +67,9 @@ function bubblesort() {
     }
 }
 
-
-//
-// quicksort
 // --------------------------------------------------------------
 function start_quicksort() {
-    let len = points.length;
+    const len = points.length;
 
     start = new Date().getTime();
     quicksort(0, len - 1);
@@ -90,21 +80,16 @@ function quicksort(left, right) {
     let fertig = true;
     let i = left;
     let j = right;
-    let tmp;
     let pivotidx = (left + right) / 2;
-    let pivot = parseInt(points[pivotidx.toFixed()]);
-    /* partition */
+    let pivot = parseInt(points[pivotidx.toFixed()]); //toFixed() = runden
+    //partition
     while (i <= j) {
-
-
         while (parseInt(points[i]) < pivot) {
-            i++;
+            i += 1;
         }
-
         while (parseInt(points[j]) > pivot) {
-            j--;
+            j -= 1;
         }
-
         if (i <= j) {
 
             gc.fillStyle = "white";
@@ -113,7 +98,7 @@ function quicksort(left, right) {
             gc.fillRect(j, points[j], 2, 2);
             gc.fillRect(j + 1, points[j + 1], 2, 2);
 
-            tmp = points[i];
+            const tmp = points[i];
             points[i] = points[j];
             points[j] = tmp;
             i++;
@@ -128,44 +113,18 @@ function quicksort(left, right) {
         }
     }
 
-
     if (fertig == false) {
         setTimeout(quicksort, 10);
     } else {
         elapsed = new Date().getTime() - start;
         document.getElementById("duration").innerHTML = "duration " + elapsed + " ms";
-        // console.log(elapsed);
     }
 
-
-    /* recursion */
+    //recursion
     if (left < j) {
         quicksort(left, j);
     }
     if (i < right) {
         quicksort(i, right);
     }
-}
-
-
-//
-// SelectionSort
-// --------------------------------------------------------------
-function start_selectionsort() {
-    alert("SelectionSort: Not implemented!");
-}
-
-function selectionsort() {
-
-}
-
-//
-// ShellSort
-// --------------------------------------------------------------
-function start_shellsort() {
-    alert("ShellSort: Not implemented!");
-}
-
-function shellsort() {
-
 }
